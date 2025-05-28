696ec9752833fbaca25aebb00da600d46f3bc13c
marzia <you@example.com>
2025-05-28 16:02:36 +0200

    Fix casing for Fizz output and correct FizzBuzz condition placement

--- js/main.js
+++ js/main.js
@@ -21,11 +21,15 @@ sicuramnete con più di singolo if
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0) {
-    console.log("fizz");
+    console.log("Fizz");
   }
 
   if (i % 5 == 0) {
     console.log("Buzz");
+  }
+
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
   } else {
     console.log(i);
   }
